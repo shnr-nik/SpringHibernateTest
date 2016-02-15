9e380c8c46c80561d73414fa245eab4060d29070
shnr-nik <Shnarevich@list.ru>
2016-02-15 12:44:46 +0300

    add lombok plugin

--- src/main/java/com/shnarevich/model/Product.java
+++ src/main/java/com/shnarevich/model/Product.java
@@ -2,6 +2,8 @@ package com.shnarevich.model;
 
 import javax.persistence.*;
 import java.util.Date;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "product")
@@ -10,43 +12,40 @@ public class Product {
     @GeneratedValue()
     private Long id;
 
-    private Integer code;
+    @Getter @Setter private Integer code;
+    @Getter @Setter private String name;
+    @Getter @Setter private Double price;
+    @Getter @Setter private Date date;
 
-    private String name;
-
-    private Double price;
-
-    private Date date;
-
-    public Integer getCode() {
-        return code;
-    }
-
-    public void setCode(Integer code) {
-        this.code = code;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Double getPrice() {
-        return price;
-    }
-
-    public void setPrice(Double price) {
-        this.price = price;
-    }
-
-    public Date getDate() {
-        return date;
-    }
-
-    public void setDate(Date date) {
-        this.date = date;
-    }
+//    public Integer getCode() {
+//        return code;
+//    }
+//
+//    public void setCode(Integer code) {
+//        this.code = code;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public Double getPrice() {
+//        return price;
+//    }
+//
+//    public void setPrice(Double price) {
+//        this.price = price;
+//    }
+//
+//    public Date getDate() {
+//        return date;
+//    }
+//
+//    public void setDate(Date date) {
+//        this.date = date;
+//    }
 }
