844c06bfa588629d08b9a41e1ff2921c14dd0a02
shnr-nik <Shnarevich@list.ru>
2016-02-15 14:24:30 +0300

    HomeController test

--- src/test/java/com/shnarevich/SpringHibernateTestApplicationTests.java
+++ src/test/java/com/shnarevich/SpringHibernateTestApplicationTests.java
@@ -2,17 +2,31 @@ package com.shnarevich;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.IntegrationTest;
+import org.springframework.boot.test.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.beans.factory.annotation.Value;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = SpringHibernateTestApp.class)
 @WebAppConfiguration
+@IntegrationTest({"server.port:0"})
 public class SpringHibernateTestApplicationTests {
+
+	@Value("${local.server.port}")
+	private int port;
 
 	@Test
 	public void contextLoads() {
+		ResponseEntity<String> entity = new TestRestTemplate()
+				.getForEntity("http://localhost:" + this.port, String.class);
+		assertThat(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
 	}
 
 }
