0191e3bd7dfcc5339098f5e859a8280797abff10
shnr-nik <Shnarevich@list.ru>
2016-02-15 16:07:04 +0300

    pom, Product

--- src/main/java/com/shnarevich/model/Product.java
+++ src/main/java/com/shnarevich/model/Product.java
@@ -7,45 +7,15 @@ import lombok.Setter;
 
 @Entity
 @Table(name = "product")
+@Getter
+@Setter
 public class Product {
     @Id
     @GeneratedValue()
     private Long id;
+    private Integer code;
+    private String name;
+    private Double price;
+    private Date date;
 
-    @Getter @Setter private Integer code;
-    @Getter @Setter private String name;
-    @Getter @Setter private Double price;
-    @Getter @Setter private Date date;
-
-//    public Integer getCode() {
-//        return code;
-//    }
-//
-//    public void setCode(Integer code) {
-//        this.code = code;
-//    }
-//
-//    public String getName() {
-//        return name;
-//    }
-//
-//    public void setName(String name) {
-//        this.name = name;
-//    }
-//
-//    public Double getPrice() {
-//        return price;
-//    }
-//
-//    public void setPrice(Double price) {
-//        this.price = price;
-//    }
-//
-//    public Date getDate() {
-//        return date;
-//    }
-//
-//    public void setDate(Date date) {
-//        this.date = date;
-//    }
 }
